781ce400afe615f72bfb17cc20d25daf413485c3
José de Jesús Acevedo Flores <jjaf051094@Laptop-de-Jose.local>
2024-10-11 11:19:51 -0300

    fix

--- src/app/challengers/layout.tsx
+++ src/app/challengers/layout.tsx
@@ -9,7 +9,7 @@ export default function ChallengersLayout({
 }) {
   return (
     
-      <main className="flex items-start justify-center min-h-screen p-[1rem] mb-[65px] bg-background-secondary mt-[119px]">
+      <main className="flex items-start justify-center min-h-[calc(100vh-191px)] p-[1rem] mb-[65px] bg-background-secondary mt-[119px]">
         <NavBar />
         <NavMain />
         {children}
--- src/app/create-post/layout.tsx
+++ src/app/create-post/layout.tsx
@@ -8,7 +8,7 @@ export default function CreatePostLayout({
   children: React.ReactNode;
 }) {
   return (
-    <main className="flex w-full items-start justify-center min-h-screen p-[1rem] mb-[65px] bg-background-secondary mt-[119px]">
+    <main className="flex w-full items-start justify-center min-h-[calc(100vh-191px)] p-[1rem] mb-[65px] bg-background-secondary mt-[119px]">
       <CampaignCoverProvider>
         <NavBar />
         <NavMain />
