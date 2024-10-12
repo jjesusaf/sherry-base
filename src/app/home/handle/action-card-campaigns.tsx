0eae488da7a7ee9b19a75a5b84f34b968491b54f
José de Jesús Acevedo Flores <jjaf051094@Laptop-de-Jose.local>
2024-10-12 18:48:46 -0300

    DD

--- src/app/home/handle/action-card-campaigns.tsx
+++ src/app/home/handle/action-card-campaigns.tsx
@@ -8,26 +8,25 @@ import { useAccount } from "wagmi";
 import { subGraphKolCampaignsByAddress } from "../actions/subgraph";
 import { useAppContext } from "@/src/context/GlobalContext";
 import { Skeleton } from "@/src/components/ui/skeleton";
-import { Button } from "@/src/components/ui/button";
 import { useRouter } from "next/navigation";
+import { Swiper, SwiperSlide } from "swiper/react";
+import "swiper/css";
+import "swiper/css/navigation";
+import "swiper/css/pagination";
+import { Navigation, Pagination, Scrollbar, Autoplay } from "swiper/modules";
+
 
 const ActionCardCampaigns: React.FC = () => {
   const router = useRouter();
   const [campaigns, setCampaigns] = useState<Campaign[]>([]);
   const { address } = useAccount();
-  const { setLoading, setIdCampaign } = useAppContext();
+  const { setLoading, setIdCampaign, idCampaign } = useAppContext();
+  const [posts, setPosts] = useState<any[]>([]);
 
   const fetchMetadataCampaigns = async (campaigns: Campaign[]) => {
-    console.log("address : ", address);
-    // Si el address no existe, el usuario no está conectado
-    // por lo que no se puede obtener las campañas suscritas
     const result = address
       ? await subGraphKolCampaignsByAddress(address)
       : null;
-
-    console.log("result: ", result);
-
-    //console.log("Campañas suscritas:", result.data.kolCampaignAddeds);
     const kolCampaign = address ? result.data.kolCampaignAddeds : [];
 
     const updatedCampaigns = await Promise.all(
@@ -43,8 +42,6 @@ const ActionCardCampaigns: React.FC = () => {
         };
       })
     );
-
-    console.log("Campañas con metadata:", updatedCampaigns);
     setCampaigns(updatedCampaigns);
   };
 
@@ -69,7 +66,7 @@ const ActionCardCampaigns: React.FC = () => {
   if (campaigns.length === 0) {
     return (
       <div className="w-full flex justify-center items-center">
-        <Skeleton className="w-[352px] h-[300px]" />;
+        <Skeleton className="w-[352px] h-[300px]" />
       </div>
     );
   }
@@ -82,14 +79,41 @@ const ActionCardCampaigns: React.FC = () => {
 
   return (
     <div className="w-full flex flex-col gap-[2rem]">
-      <section className="flex flex-col gap-[1rem]">
+      <section className="flex flex-col gap-[1rem] m-0">
         <h2 className="text-xl font-semibold text-foreground">My Challenges</h2>
         {subscribedCampaigns.length > 0 ? (
-          <div className="w-full flex flex-wrap justify-center md:justify-start items-center gap-[30px]">
+          <Swiper
+            modules={[Navigation, Pagination, Scrollbar, Autoplay]}
+            spaceBetween={0}
+            slidesPerView={1}
+            navigation={{
+              nextEl: ".swiper-button-next",
+              prevEl: ".swiper-button-prev",
+            }}
+            autoplay={{
+              delay: 2000,
+              disableOnInteraction: false,
+            }}
+            pagination={{ clickable: true }}
+            breakpoints={{
+              640: {
+                slidesPerView: 1,
+              },
+              768: {
+                slidesPerView: 2,
+              },
+              1024: {
+                slidesPerView: 3,
+              },
+            }}
+            className="w-full"
+          >
             {subscribedCampaigns.map((campaign, key) => (
-              <CardCampaigns key={key} campaign={campaign} />
+              <SwiperSlide key={key}>
+                <CardCampaigns campaign={campaign} />
+              </SwiperSlide>
             ))}
-          </div>
+          </Swiper>
         ) : (
           <p className="text-gray-500">
             You are not subscribed to any challenges yet.
@@ -98,9 +122,7 @@ const ActionCardCampaigns: React.FC = () => {
       </section>
 
       <section className="flex flex-col gap-[1rem]">
-        <h2 className="text-xl font-semibold text-foreground">
-          Discover More
-        </h2>
+        <h2 className="text-xl font-semibold text-foreground">Discover More</h2>
         {discoverCampaigns.length > 0 ? (
           <div className="w-full flex flex-wrap justify-center md:justify-start items-center gap-[30px]">
             {discoverCampaigns.map((campaign, key) => (
