4c4cde554e0412f8cde583223b683c51f66a0718
José de Jesús Acevedo Flores <jjaf051094@Laptop-de-Jose.local>
2024-10-14 11:23:48 -0300

    f

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -47,7 +47,17 @@ export default function RootLayout({
         <OnchainProviders>
           <AppProvider>
             <ClientLayoutWithSpinner>
-              <NextTopLoader />
+              <NextTopLoader
+                color="#CB1D63"
+                initialPosition={0.08}
+                crawlSpeed={200}
+                height={3}
+                crawl={true}
+                showSpinner={true}
+                easing="ease"
+                speed={200}
+                shadow="0 0 10px #e11d48,0 0 5px #CB1D63"
+              />
               {children}
             </ClientLayoutWithSpinner>
             <Toaster />
--- src/components/global/nav-bar-home.tsx
+++ src/components/global/nav-bar-home.tsx
@@ -1,16 +1,27 @@
+"use client";
 import React from "react";
 import TeamSwitcher from "./team-switcher";
 import { Menu } from "lucide-react";
 import Image from "next/image";
 import SignupButton from "../SignupButton";
 import { Avatar, Name } from "@coinbase/onchainkit/identity";
+import { StarIcon, Wallet2Icon } from "lucide-react";
+import { useAccount } from "wagmi";
 
 const NavBarHome: React.FC = () => {
+  const { address } = useAccount();
   return (
-    <div className="fixed top-0 left-0 flex w-full justify-between items-center py-[0.5rem] z-10 px-[1rem] border border-border rounded-t-lg shadow-md bg-background">
+    <div className="fixed top-0 left-0 flex w-full justify-between items-center py-[0.5rem] z-10 px-[1rem] border border-border rounded-t-lg shadow-md bg-background ">
       <Menu className="w-6 h-6 text-foreground" />
       <Image src="/images/icon.svg" alt="logo" width={24} height={24} />
-      <div className=" rounded-full border border-border items-center justify-center flex">
+      <div className="rounded-full border border-border items-center justify-center flex relative">
+        <div className="absolute inset-0 flex items-center justify-center">
+          {address ? (
+            <StarIcon className="h-[14px] w-[14px]" />
+          ) : (
+            <Wallet2Icon className="h-[24px] w-[24px] hover:text-crimson11" />
+          )}
+        </div>
         <SignupButton />
       </div>
     </div>
